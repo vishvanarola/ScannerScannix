f5143eb2db41bbb713b02c86301ed0ec43e6d52e
Vishva Narola <vishvanarola02@gmail.com>
2025-05-23 10:20:29 +0530

    Set up project for package

---
+++ ScannixScanner/ScannixScanner.h
@@ -0,0 +1,18 @@
+//
+//  ScannixScanner.h
+//  ScannixScanner
+//
+//  Created by apple on 22/05/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScannixScanner.
+FOUNDATION_EXPORT double ScannixScannerVersionNumber;
+
+//! Project version string for ScannixScanner.
+FOUNDATION_EXPORT const unsigned char ScannixScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScannixScanner/PublicHeader.h>
+
+
